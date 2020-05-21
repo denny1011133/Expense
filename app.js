415035d90c4f75110176ab20651063d4d6112fb6
Denny <denny1011133@gmail.com>
2020-05-21 22:16:15 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -11,3 +11,17 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
   console.log('App is running on http://localhost:3000')
 })
+
+const mongoose = require('mongoose') // 載入 mongoose
+mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
+
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
