d066f2e692bf5ec03cfe6cd88f043fd8102d5f59
Denny <denny1011133@gmail.com>
2020-05-22 19:29:09 +0800

    feat: add create function

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars');
 const Record = require('./models/record')
+const bodyParser = require('body-parser')
 const app = express()
 mongoose.connect('mongodb://localhost/Expense', { useNewUrlParser: true, useUnifiedTopology: true })
 
@@ -17,14 +18,25 @@ db.once('open', () => {
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
-
+app.use(bodyParser.urlencoded({ extended: true }))
+//首頁
 app.get('/', (req, res) => {
   Record.find()
     .lean()
     .then(records => res.render('index', { records }))
     .catch(error => console.error(error))
 })
-
+//新增資料頁面
+app.get('/records/new', (req, res) => {
+  return res.render('new')
+})
+//新增一筆資料
+app.post('/todos', (req, res) => {
+  const name = req.body.name       // 從 req.body 拿出表單裡的 name 資料
+  return Todo.create({ name })     // 存入資料庫
+    .then(() => res.redirect('/')) // 新增完成後導回首頁
+    .catch(error => console.log(error))
+})
 app.listen(3000, () => {
   console.log('App is running on http://localhost:3000')
 })
