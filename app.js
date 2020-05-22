0c34d6e8e55a5a16b3d12446c7068333594555de
Denny <denny1011133@gmail.com>
2020-05-22 19:47:11 +0800

    fix: add create function

--- app.js
+++ app.js
@@ -31,10 +31,11 @@ app.get('/records/new', (req, res) => {
   return res.render('new')
 })
 //新增一筆資料
-app.post('/todos', (req, res) => {
-  const name = req.body.name       // 從 req.body 拿出表單裡的 name 資料
-  return Todo.create({ name })     // 存入資料庫
-    .then(() => res.redirect('/')) // 新增完成後導回首頁
+app.post('/records', (req, res) => {
+  const { name, category, amount, date } = req.body
+  console.log(req.body)
+  return Record.create({ name, category, amount, date })
+    .then(() => res.redirect('/'))
     .catch(error => console.log(error))
 })
 app.listen(3000, () => {
--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -8,27 +8,27 @@ db.on('error', () => {
 db.once('open', () => {
   Record.create({
     name: "午餐",
-    category: `<i class="fas fa-utensils"></i>`,
+    category: '餐飲食品',
     date: "2019/04/23",
     amount: 60,
   }, {
     name: "晚餐",
-    category: `<i class="fas fa-utensils"></i>`,
+    category: '家居物業',
     date: "2019/04/23",
     amount: 60,
   }, {
     name: "捷運",
-    category: `<i class="fas fa-shuttle-van"></i>`,
+    category: '交通出行',
     date: "2019/04/23",
     amount: 120,
   }, {
     name: "電影: 驚奇隊長",
-    category: `<i class="fas fa-grin-beam"></i>`,
+    category: '休閒娛樂',
     date: "2019/04/23",
     amount: 220,
   }, {
     name: "租金",
-    category: `<i class="fas fa-home"></i>`,
+    category: '家居物業',
     date: "2019/04/01",
     amount: 25000,
   })
