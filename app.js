4c518dcd3e27618dd433f5783a3dbf6ad3f85db1
Denny <denny1011133@gmail.com>
2020-05-22 14:40:25 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,20 +1,9 @@
-// 載入 express 並建構應用程式伺服器
 const express = require('express')
-const app = express()
-
-// 設定首頁路由
-app.get('/', (req, res) => {
-  res.send('hello world')
-})
-
-// 設定 port 3000
-app.listen(3000, () => {
-  console.log('App is running on http://localhost:3000')
-})
-
 const mongoose = require('mongoose') // 載入 mongoose
-mongoose.connect('mongodb://localhost/Expense', { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
+const exphbs = require('express-handlebars');
 
+const app = express()
+mongoose.connect('mongodb://localhost/Expense', { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
 // 取得資料庫連線狀態
 const db = mongoose.connection
 // 連線異常
@@ -24,4 +13,19 @@ db.on('error', () => {
 // 連線成功
 db.once('open', () => {
   console.log('mongodb connected!')
+})
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+// 設定首頁路由
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+
+
+// 設定 port 3000
+app.listen(3000, () => {
+  console.log('App is running on http://localhost:3000')
 })
