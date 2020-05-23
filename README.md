01e6bdf38f21efb2aa11127b730aad8581099c27
Denny <denny1011133@gmail.com>
2020-05-23 13:03:58 +0800

    add README.md

---
+++ README.md
@@ -0,0 +1,15 @@
+# 簡易記帳本
+使用Node.js，並載入Express、 express-handlebars、mongoose、bodyParser...等相關套件。
+# 使用版本
+* Node.js v10.15.0 
+* Express v4.17.1 
+* express-handlebars v4.0.4 
+* mongoose v5.9.14
+* bodyParser v1.19.0
+
+# 網站使用功能
+* 查看所有支出的相關資訊(支出名稱、類型、日期)
+* 分類各項支出
+* 新增單筆支出資訊
+* 刪除單筆支出資訊
+
--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const routes = require('./routes')
 require('./config/mongoose')
-const PORT = process.env.PORT || 3000
-const app = express()
 
+const app = express()
+const PORT = process.env.PORT || 3000
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
@@ -14,6 +14,6 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(routes)
 
-app.listen(process.env.PORT, () => {
-  console.log('App is running on http://localhost:${process.env.PORT}')
+app.listen(PORT, () => {
+  console.log(`App is running on http://localhost:${PORT}`)
 })
