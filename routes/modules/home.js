adfcbe94e854953e5b461f375a40477034dda04d
Denny <denny1011133@gmail.com>
2020-06-15 20:22:33 +0800

    fix selected function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -14,8 +14,8 @@ router.get('/', (req, res) => {
     })
     .catch(error => console.error(error))
 })
-//首頁過濾月份
-router.post('/month', (req, res) => {
+//首頁過濾月份與分類
+router.post('/', (req, res) => {
   const userId = req.user._id
   let totalAmount = 0
   const categoryInChinese = {
@@ -23,11 +23,26 @@ router.post('/month', (req, res) => {
     '<i class="fas fa-shuttle-van"></i>': "交通出行",
     '<i class="fas fa-grin-beam"></i>': "休閒娛樂",
     '<i class="fas fa-utensils"></i>': "餐飲食品",
-    '<i class="fas fa-pen"></i>': "其他"
+    '<i class="fas fa-pen"></i>': "其他",
   }
-  const categorySelected = req.body.categorySelect
-  const monthSelected = req.body.month
-  if (monthSelected) {
+  const categorySelected = req.body.categorySelect //<i class="fas fa-home"></i>
+  const monthSelected = req.body.month //2020-06
+  if (categorySelected !== "ALL" && monthSelected) {
+    Record.find({ category: categorySelected, userId })
+      .lean()
+      .then(records => {
+        return records.filter(record => record.date.includes(monthSelected)
+        )
+      })
+      .then(records => {
+        const categoryInChineseSelected = categoryInChinese[categorySelected]
+        records.forEach(record => {
+          totalAmount += record.amount
+        })
+        res.render('index', { records, totalAmount, categoryInChineseSelected, monthSelected })
+      })
+      .catch(error => console.error(error))
+  } else if (categorySelected === "ALL" && monthSelected) {
     Record.find({ userId })
       .lean()
       .then(records => {
@@ -35,49 +50,17 @@ router.post('/month', (req, res) => {
         )
       })
       .then(records => {
-        records.forEach(record => {
-          totalAmount += record.amount
-        })
-        return res.render('index', { records, monthSelected, totalAmount })
+        const categoryInChineseSelected = "所有支出"
+        records.forEach(record => totalAmount += record.amount)
+        res.render('index', { records, totalAmount, categoryInChineseSelected, monthSelected })
       })
       .catch(error => console.error(error))
   }
 })
 
-//首頁過濾類型
-router.post('/category', (req, res) => {
-  const userId = req.user._id
-  let totalAmount = 0
-  const categoryInChinese = {
-    '<i class="fas fa-home"></i>': "家居物業",
-    '<i class="fas fa-shuttle-van"></i>': "交通出行",
-    '<i class="fas fa-grin-beam"></i>': "休閒娛樂",
-    '<i class="fas fa-utensils"></i>': "餐飲食品",
-    '<i class="fas fa-pen"></i>': "其他"
-  }
-  const categorySelected = req.body.categorySelect
 
-  if (categorySelected !== "ALL") {
-    Record.find({ category: categorySelected, userId })
-      .lean()
-      .then(records => {
-        const categoryInChineseSelected = categoryInChinese[categorySelected]
-        records.forEach(record => {
-          totalAmount += record.amount
-        })
-        res.render('index', { records, totalAmount, categoryInChineseSelected })
-      })
-      .catch(error => console.error(error))
-  } else Record.find()
-    .lean()
-    .then(records => {
-      const categoryInChineseSelected = "所有支出"
-      records.forEach(record => totalAmount += record.amount)
-      res.render('index', { records, totalAmount, categoryInChineseSelected })
-    })
-    .catch(error => console.error(error))
-})
 
 module.exports = router
+
 
 
